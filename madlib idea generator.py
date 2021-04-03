1c5ae566a57c3f2e5816133e3142a3654b207320
Akshay-Tari <63060009+Akshay-Tari@users.noreply.github.com>
2021-04-03 20:22:44 +0530

    Add files via upload

---
+++ madlib idea generator.py
@@ -0,0 +1,9 @@
+
+import random
+
+everyone_need = ['money','mobile','happy life ','medicine','healthy life ','time ', 'freedom', 'world journey','fiancial freedom ', 'food', 'water']
+there_current_option = ['work hard','networking', 'creative idea', 'roadmap','business', 'IOT ','nature' ]
+new_technology =['bitcoin','IOT','blockchain', 'AI', 'virtual relaity','green technoly ','cyber world', 'smart anteena', 'predective']
+
+print('everyone need',random.choice(everyone_need) + ',' + 'there current option',random.choice(there_current_option)+  ',' + 'new technology',random.choice(new_technology))
+
